7c728237b07b81f80c0ea160f0aa69175c843434
minhoshin11 <tlsalsgh777@naver.com>
2024-01-25 19:31:30 +0900

    Develope auto-system

--- src/App-copy.js
+++ src/App-copy.js
@@ -10,37 +10,17 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState(false); 
   
- //왼쪽(따봉)은 변수명 , 오른쪽(c)는 실행시킬 함수
   return (
     <div className="App">
     <div className="black-nav">
     <h4>ReactBlog</h4>
     </div>
     <div className='list'>
-      {/* 정렬버튼 만드는 것임. */}
     <button onClick={ () =>{
       let copy = [...글제목];
       copy.sort();
       글제목변경(copy)
     }}>정렬버튼</button>
-
-
-      {/* <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
-      <p>2월 17일 발행</p>
-      <button onClick={() => {
-        let copy = [...글제목];
-        copy[0] = '여자 코트 추천';
-        글제목변경(copy);
-      }}>추천변경</button>
-    </div>
-    <div className='list'>
-      <h4>{ 글제목[1] }</h4>
-      <p>2월 17일 발행</p>
-    </div>
-    <div className='list'>
-      <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}
-      </h4>
-    <p>2월 17일 발행</p>  */}
     </div> 
     
 
@@ -54,7 +34,7 @@ function App() {
         글제목.map(function(a, i){
           return(
       <div className='list' key={i}>
-      <span onClick={()=>{setModal(!modal)}}> { 글제목[i] } </span>
+      <h4 onClick={()=>{setModal(!modal)}}> { 글제목[i] } </h4>
       <span onClick={()=>{ 
       let copy = [...따봉];
       copy[i] = copy[i] + 1;
@@ -80,7 +60,7 @@ function Modal(){
       <p>상세내용</p>
     </div>
     )
-} //리턴을 만들 때 하나의 div만 만들어야함. 두개이상 존재하면 오류뜸
+}
 
 
 }
