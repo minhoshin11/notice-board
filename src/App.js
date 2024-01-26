67e1fc29aa00f3a393fb3271988d610fa003a722
minhoshin11 <tlsalsgh777@naver.com>
2024-01-26 19:43:12 +0900

    add props

--- src/App.js
+++ src/App.js
@@ -23,33 +23,9 @@ function App() {
       copy.sort();
       글제목변경(copy)
     }}>정렬버튼</button>
-
-
-      {/* <h4>{ 글제목[0] } <span onClick={()=>{따봉변경(따봉+1)}}>👍</span> {따봉} </h4>
-      <p>2월 17일 발행</p>
-      <button onClick={() => {
-        let copy = [...글제목];
-        copy[0] = '여자 코트 추천';
-        글제목변경(copy);
-      }}>추천변경</button>
-    </div>
-    <div className='list'>
-      <h4>{ 글제목[1] }</h4>
-      <p>2월 17일 발행</p>
-    </div>
-    <div className='list'>
-      <h4 onClick={()=>{setModal(!modal)}}>{글제목[2]}
-      </h4>
-    <p>2월 17일 발행</p>  */}
     </div> 
     
 
-
-      {
-        modal == true ? <Modal></Modal> : null
-      }
-      
-      
       {      
         글제목.map(function(a, i){
           return(
@@ -64,24 +40,24 @@ function App() {
       <p>2월 17일 발행</p>
     </div> )})
       }
+
+{
+        modal == true ? <Modal color="skyblue" 글제목={글제목}/> : null
+      }
       
-    </div>
+      </div>
   )
 
-
-
-
-
-function Modal(){
+  function Modal(props){
     return(
-      <div className="modal">
-      <h4>제목</h4>
+      <div className="modal" style={{backgroundColor : props.color}}>
+      <h4>{props.글제목}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button>글수정</button>
     </div>
     )
 } //리턴을 만들 때 하나의 div만 만들어야함. 두개이상 존재하면 오류뜸
-
 
 }
 
